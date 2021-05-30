2cb365f802d7068f68f31da173ea89e0bdf76e79
Nopphon <nopphonyel@gmail.com>
2021-05-30 13:06:52 +0700

    Wavelet Convolution not finish yet

--- model/module/wavelet_func.py
+++ model/module/wavelet_func.py
@@ -1,10 +1,10 @@
 import torch
 
 
-def kernel_build(k_s, start, end):
-    return torch.linspace(start, end, k_s)
+def kernel_build(k_s, start, end, in_ch, trn, dil, bs=1):
+    k = torch.linspace(start, end, k_s).expand(bs, )
+    return k
 
 
-def mexican_hat(z, trn=1, dil=1):  # Tested and yeah... I think its correct
-    #z = (z - trn) / dil
+def mexican_hat(z):  # Tested and yeah... I think its correct
     return (1 - torch.pow(z, 2)) * torch.exp(torch.Tensor([-0.5]) * torch.pow(z, 2))
